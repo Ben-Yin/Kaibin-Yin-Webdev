40d97a544ee7c64e2e94c92a002dbc367bd8de04
Ben-Yin <BenYin@Kaibins-MacBook-Pro.local>
2017-01-21 14:29:37 -0500

    finish angular user.controller

---
+++ public/assignment/views/user/user.controller.client.js
@@ -0,0 +1,16 @@
+(function() {
+    angular
+        .module("WebAppMaker")
+        .controller("LoginController", LoginController)
+        .controller("RegisterController", RegisterController)
+        .controller("ProfileController", ProfileController)
+    function LoginController() {
+        var vm = this;
+    }
+    function RegisterController() {
+        var vm = this;
+    }
+    function ProfileController() {
+        var vm = this;
+    }
+})();
